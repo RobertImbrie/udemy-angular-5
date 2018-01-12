c63f1dbf347888661bae4798bc20e57acacf031c
RobertImbrie <robert.m.imbrie@gmail.com>
2018-01-12 12:47:30 -0600

    Add WarningAlert component with styling

---
+++ basics-assignment-1-start/src/app/WarningAlert/warningalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'warning-alert-component',
+  templateUrl: './warningalert.component.html',
+  styleUrls: ['./warningalert.component.css']
+})
+export class WarningAlertComponent {
+}
--- basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { WarningAlertComponent } from './WarningAlert/warningalert.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
