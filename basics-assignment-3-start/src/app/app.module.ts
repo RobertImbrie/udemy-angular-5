1b389995c5cf611367cbc83a8badcbdd823892ef
RobertImbrie <robert.m.imbrie@gmail.com>
2018-01-13 12:58:09 -0600

    Add log component via CLI

--- basics-assignment-3-start/src/app/app.module.ts
+++ basics-assignment-3-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DisplayComponent } from './display/display.component';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DisplayComponent
+    DisplayComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
