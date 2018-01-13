262bc7c804efa394d89b2288c3fa433364554378
RobertImbrie <robert.m.imbrie@gmail.com>
2018-01-13 12:18:34 -0600

    Use ngFor directive
    -Use ngFor to iterate over the 'servers' array in the servers component. For each entry, create a new server paragraph

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
 	serverCreationStatus = 'No server was created!'
 	serverName = 'Test server';
 	serverCreated = false;
+	servers = ['restserver', 'testserver2'];
 
   constructor() { 
   	setTimeout( ()=> {
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
   	this.serverCreated = true;
+  	this.servers.push( this.serverName );
   	this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
