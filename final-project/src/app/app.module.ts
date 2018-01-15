a86053ad20ec67f5364d9c041cf2207b368b41a1
RobertImbrie <robert.m.imbrie@gmail.com>
2018-01-15 10:45:50 -0600

    Move shopping list to subfolder

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
     RecipeListComponent,
     RecipesComponent,
     RecipeDetailComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
