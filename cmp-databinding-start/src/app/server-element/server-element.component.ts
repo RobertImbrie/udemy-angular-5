002f6d6ebf0005ac6d68c82c7f14f856a788a5a1
RobertImbrie <robert.m.imbrie@gmail.com>
2018-04-28 10:14:01 -0500

    Using local references to pass data one way without having to modify TypeScript code

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Native
+  styleUrls: ['./server-element.component.css']
+  //encapsulation: ViewEncapsulation.Native
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
