8932beef3680a1c26f27d901dc449beac7b28c8b
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-12 07:57:27 -0500

    Removing some files

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    oddElements = [];
+    evenElements = [];
+
+    onStartClicked( numberSeconds ) {
+      console.log(numberSeconds);
+      if ((numberSeconds % 2) === 1) {
+        this.oddElements.push(numberSeconds);
+      } else {
+       this.evenElements.push(numberSeconds);
+      }
+    }
+
+    onStopClicked() {
+    }
 }
