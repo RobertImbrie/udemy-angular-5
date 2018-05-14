5663efe746e0a3fb64f27283d32186debd861fd4
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-14 08:08:50 -0500

    Add items to shopping list via ShoppingListService

--- prj-basics-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-basics-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,7 +9,15 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
+
+  addToList() {
+    this.recipe.ingredients.forEach(
+      function(ingredient) {
+        this.slService.add(ingredient);
+      }
+    );
+  }
 
   ngOnInit() {
   }
