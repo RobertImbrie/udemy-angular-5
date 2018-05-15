aad4e65d52ac3bb42a794f844297f9a17e0a89d9
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-15 14:02:04 -0500

    Using a resolver instead of params in the server routing, which is useful for asynchronous data

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
+import { ServerResolver } from './servers/server/server-resolver.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
 
@@ -36,7 +37,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuard],
+  providers: [ServersService, AuthService, AuthGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
