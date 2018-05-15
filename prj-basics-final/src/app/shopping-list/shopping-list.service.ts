497582dda1a59c10f124e3224a2b4ea96abd957b
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-15 18:58:38 -0500

    Replacing EventEmitters with subjects

--- prj-basics-final/src/app/shopping-list/shopping-list.service.ts
+++ prj-basics-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -10,17 +10,17 @@ export class ShoppingListService {
 
       add( ingredient: Ingredient ) {
         this.ingredients.push( ingredient );
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
       }
 
       addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
       }
 
       delete() {
         this.ingredients.pop();
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
       }
 
       clear() {
