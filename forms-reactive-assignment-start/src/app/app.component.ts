f933eff1390223da94489514795299294ed84af9
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-17 18:40:02 -0500

    Adding typescript form controls, asychronous form validation

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null),
+      'email': new FormControl(null, Validators.email),
+      'projectStatus': new FormControl(null)
+    });
+  }
+
+  asynchValidateProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      if (control.value === 'Test') {
+        resolve({'projectNameIsForbidden': true})
+        } else {
+          resolve(null);
+        }
+      }
+    );
+    return promise;
+  }
 }
