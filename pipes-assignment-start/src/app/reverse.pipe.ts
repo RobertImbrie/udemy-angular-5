57181e2310d5c540c006fb2b197f7e32296ed709
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-18 13:39:03 -0500

    Implemented 'reverse' pipe for strings with built in Javascript functions

--- pipes-assignment-start/src/app/reverse.pipe.ts
+++ pipes-assignment-start/src/app/reverse.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReversePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string, args?: any): any {
+    return value.split('').reverse().join('');
   }
 
 }
