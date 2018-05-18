09312b01ec8f6922887469a486f6a1a665687ed0
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-18 13:51:42 -0500

    Implemented pipe to sort an array of objects via nested ternary operators comparing their names

--- pipes-assignment-start/src/app/sort.pipe.ts
+++ pipes-assignment-start/src/app/sort.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any[], args?: any): any {
+    console.log(value);
+    value.sort( (a: any, b: any) => {
+      return a.name < b.name ? -1 :
+        (a.name > b.name ? 1 : 0);
+    });
+    return value;
   }
 
 }
