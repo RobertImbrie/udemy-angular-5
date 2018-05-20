c26b87f549edfb738a917736c77847d3a65a978b
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-20 11:25:01 -0500

    Form now properly resets after adding/editing shopping ingredients

--- prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-observables-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -44,7 +44,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -52,6 +52,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    form.resetForm();
+    this.editMode = false;
   }
 
 }
