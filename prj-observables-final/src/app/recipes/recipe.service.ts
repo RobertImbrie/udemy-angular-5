0170abb06743b7180026a2a8a268316972e5d48c
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-20 17:37:00 -0500

    Recipe Edit form now submits properly

--- prj-observables-final/src/app/recipes/recipe.service.ts
+++ prj-observables-final/src/app/recipes/recipe.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -40,7 +42,17 @@ export class RecipeService {
   }
 
   resetRecipes(recipes: Recipe[]) {
-    console.log( recipes );
     this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
