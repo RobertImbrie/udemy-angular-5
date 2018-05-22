46ee35cc36c66b89721455cf5c920cf66d482192
RobertImbrie <robert.m.imbrie@gmail.com>
2018-05-22 08:55:29 -0500

    Installed Firebase SDK with NPM into the project, initialized firebase in the app.component file with the retrieved apiKey/authDomain

--- prj-observables-final/src/app/app.component.ts
+++ prj-observables-final/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      'apiKey': 'AIzaSyAD08i9AoF-mKw6yC87AcU9f2MqaVPFwOU',
+      'authDomain': 'udemy-ng-http-510bf.firebaseapp.com'
+    });
   }
 }
